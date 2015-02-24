c92fcee57ba896a7d2b7886a80edc407526f59a8
khsushan <khsushan@gmail.com>
2015-02-24 10:55:27 +0530

    mobile application added

--- my_script.sh
+++ my_script.sh
@@ -32,26 +32,49 @@ travelAppwar=${prop_value}
 getProperty "travel_booking_war_file"
 travelBookingAppwar=${prop_value} 
 
+getProperty "mobile_application_folder"
+mobile_application_folderPath=${prop_value} 
+echo "Mobile application folder path : "$mobile_application_folderPath
+
+#notifi_webapp_folder
+getProperty "notifi_webapp_folder"
+notifi_application_folderPath=${prop_value} 
+
+getProperty "lamp_zip_file"
+lamp_zip_file_path=${prop_value}
+
 unzipFile(){
 	unzip $1 -d $2	
 }
 
+#setting up servers
 unzipFile $tomcatPackPath $tomcatPath
+sudo chmod 777 -R /opt
+unzipFile $lamp_zip_file_path "/opt"
 unzipFile $appmPackPath $appmPath
-unzipFile $travelAppwar $tomcatPath"/apache-tomcat-7.0.59/webapps/plan-your-trip"
+#deploying web application samples
+FILENAME=${tomcatPackPath##*/}
+tomcat_name=${FILENAME%.*}
+unzipFile $travelAppwar $tomcatPath"/"$tomcat_name"/webapps/plan-your-trip"
 echo "Travel Application deployed"
-unzipFile $travelBookingAppwar $tomcatPath"/apache-tomcat-7.0.59/webapps/tavel-booking"	
+unzipFile $travelBookingAppwar $tomcatPath"/"$tomcat_name"/webapps/tavel-booking"	
 echo "Travel Booking Application deployed"
-
+sudo chmod 777 -R /opt/lampp/htdocs
+cp -R $notifi_application_folderPath "/opt/lampp/htdocs"
+echo "notifi Application deployed"
+#deploying mobile applications
+FILENAME=${appmPackPath##*/}
+appm_name=${FILENAME%.*}
+cp -R $mobile_application_folderPath $appmPath"/"$appm_name"/repository/resources"
+#starting servers
 echo "Starting tomcat......"
-sudo chmod 755 -R $tomcatPath"/apache-tomcat-7.0.59/bin"
-sh $tomcatPath"/apache-tomcat-7.0.59/bin/startup.sh"
-
+sudo chmod 755 -R $tomcatPath"/"$tomcat_name"/bin"
+sh $tomcatPath"/"$tomcat_name"/bin/startup.sh"
+sudo /opt/lampp/lampp start
 echo "Starting app manager......"
-sudo chmod 755 -R $appmPath"/wso2appm-1.0.0-SNAPSHOT/bin"
-
-gnome-terminal -e  "sh $appmPath/wso2appm-1.0.0-SNAPSHOT/bin/wso2server.sh"
-
-java -cp /home/ushan/IdeaProjects/APPM_Startup/target/APPM_Startup-1.0-SNAPSHOT-jar-with-dependencies.jar com.wso2.main.MainController
+sudo chmod 755 -R $appmPath"/"$appm_name"/bin"
+gnome-terminal -e  "sh $appmPath/$appm_name/bin/wso2server.sh"
+#run java programme
+java -cp ./APPM_Startup-1.0-SNAPSHOT-jar-with-dependencies.jar com.wso2.main.MainController 8080 80 $appmPath"/"$appm_name
 
 echo "webapps deployed..."
